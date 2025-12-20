72e4239945bf1ec1f8a902943a8796ee16203d1a
Markus Goetz <57562344+MythicByte@users.noreply.github.com>
2025-12-20 23:33:53 +0100

    Added Final Sqlite and the connection to it

--- src/interface.rs
+++ src/interface.rs
@@ -1,13 +1,17 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, str::FromStr};
 
+use anyhow::Context;
 use chrono::{DateTime, Utc};
 use iced::{widget::container, Element};
 use openssl::x509::X509;
+use sqlx::{Database, Pool, Sqlite, SqlitePool, sqlite::SqlitePoolOptions};
+use tokio::runtime::Runtime;
 
 #[derive(Debug)]
 pub struct App {
     screen: Screen,
     connect_values: Option<ConnectValues>,
+    sqlite_pool: Pool<Sqlite>
 }
 #[derive(Debug)]
 struct ConnectValues {
@@ -24,6 +28,27 @@ pub enum Screen {
     Home,
 }
 impl App {
+    pub fn new() -> Self {
+        Self::new_result().expect("Setting up failed")
+    }
+    fn new_result() -> anyhow::Result<Self> {
+        let rt = Runtime::new().context("The tokio Runtime Failed")?;
+        let sqlite = sqlx::sqlite::SqliteConnectOptions::from_str("sqlite:otr_demeo.db")?
+            .create_if_missing(true);
+        let pool = rt.block_on(async {
+            let pool = SqlitePoolOptions::new()
+            .max_connections(2)
+            .connect_with(sqlite)
+            .await.expect("Sqlite Pool failed");
+        sqlx::migrate!("./migrations").run(&pool).await.expect("Sqlite Migrations failed");
+        pool
+        });
+        Ok(Self {
+            screen: Screen::Home,
+            connect_values: None,
+            sqlite_pool: pool,
+        })
+    }
     pub fn update(&mut self, message: Message) {
         match message {
             Message::MessageInsert(text_message, date_time, user_id) => {
@@ -42,13 +67,5 @@ impl App {
     }
     fn home(&self) -> Element<'_, Message> {
         container("Test").into()
-    }
-}
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            screen: Screen::Home,
-            connect_values: None,
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,21 @@
 pub mod diffie_hellman;
 pub mod interface;
 pub mod message;
+use anyhow::Context;
 use iced::Theme;
 use interface::App;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 fn main() -> iced::Result {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Tracing Subscriber failed to setup");
+
+    info!("Application ist starting");
     openssl::init();
-    iced::application(App::default, App::update, App::view)
+    iced::application(App::new, App::update, App::view)
         .theme(Theme::Dark)
         .title("OTRv1 Demo Project")
         .run()
